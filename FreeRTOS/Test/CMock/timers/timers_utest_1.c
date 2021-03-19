eb03ba84aceac5db75daf24940606601302c5563
Alfred Gedeon <alfred2g@hotmail.com>
2021-03-19 13:56:58 -0700

    UnitTest: timers.c

--- FreeRTOS/Test/CMock/timers/timers_utest_1.c
+++ FreeRTOS/Test/CMock/timers/timers_utest_1.c
@@ -40,13 +40,55 @@
 #include "mock_queue.h"
 #include "mock_list.h"
 #include "mock_fake_assert.h"
+#include "mock_portable.h"
+
+/* ======================  DEFINITIONS FROM timers.c ======================== */
+#define tmrNO_DELAY    ( TickType_t ) 0U
+
+#define tmrSTATUS_IS_ACTIVE                  ( ( uint8_t ) 0x01 )
+#define tmrSTATUS_IS_STATICALLY_ALLOCATED    ( ( uint8_t ) 0x02 )
+#define tmrSTATUS_IS_AUTORELOAD              ( ( uint8_t ) 0x04 )
+
+typedef struct tmrTimerControl                  /* The old naming convention is used to prevent breaking kernel aware debuggers. */
+{
+    const char * pcTimerName;                   /*<< Text name.  This is not used by the kernel, it is included simply to make debugging easier. */ /*lint !e971 Unqualified char types are allowed for strings and single characters only. */
+    ListItem_t xTimerListItem;                  /*<< Standard linked list item as used by all kernel features for event management. */
+    TickType_t xTimerPeriodInTicks;             /*<< How quickly and often the timer expires. */
+    void * pvTimerID;                           /*<< An ID to identify the timer.  This allows the timer to be identified when the same callback is used for multiple timers. */
+    TimerCallbackFunction_t pxCallbackFunction; /*<< The function that will be called when the timer expires. */
+    #if ( configUSE_TRACE_FACILITY == 1 )
+        UBaseType_t uxTimerNumber;              /*<< An ID assigned by trace tools such as FreeRTOS+Trace */
+    #endif
+    uint8_t ucStatus;                           /*<< Holds bits to say if the timer was statically allocated or not, and if it is active or not. */
+} xTIMER;
+
+typedef xTIMER Timer_t;
 
 
 /* ============================  GLOBAL VARIABLES =========================== */
 static uint16_t usMallocFreeCalls = 0;
+static uint32_t critical_section_counter;
 
+/* =============================  FUNCTION HOOKS  =========================== */
+void vFakePortEnterCriticalSection( void )
+{
+    critical_section_counter++;
+}
+
+void vFakePortExitCriticalSection( void )
+{
+    critical_section_counter--;
+}
+
+void vFakePortYieldWithinAPI()
+{
+ //   HOOK_DIAG();
+//    port_yield_within_api_called = true;
+ //   py_operation();
+}
 /* ==========================  CALLBACK FUNCTIONS =========================== */
 
+/*
 void * pvPortMalloc( size_t xSize )
 {
     return unity_malloc( xSize );
@@ -55,6 +97,7 @@ void vPortFree( void * pv )
 {
     return unity_free( pv );
 }
+*/
 
 /*******************************************************************************
  * Unity fixtures
@@ -62,9 +105,9 @@ void vPortFree( void * pv )
 void setUp( void )
 {
     vFakeAssert_Ignore();
-
     /* Track calls to malloc / free */
     UnityMalloc_StartTest();
+    critical_section_counter = 0;
 }
 
 /*! called before each testcase */
@@ -101,9 +144,13 @@ void test_xTimerCreate_Success( void )
 {
     uint32_t ulID = 0;
     TimerHandle_t xTimer = NULL;
+    Timer_t  pxNewTimer;
+    QueueHandle_t queue_handlek;
 
-    vListInitialise_Ignore();
-    xQueueGenericCreateStatic_IgnoreAndReturn( ( QueueHandle_t ) 1 );
+    pvPortMalloc_ExpectAndReturn( sizeof(Timer_t), &pxNewTimer );
+    vListInitialise_ExpectAnyArgs();
+    vListInitialise_ExpectAnyArgs();
+    xQueueGenericCreateStatic_ExpectAnyArgsAndReturn( queue_handle );
     vQueueAddToRegistry_Ignore();
     vListInitialiseItem_Ignore();
 
@@ -114,9 +161,44 @@ void test_xTimerCreate_Success( void )
                            xCallback_Test );
 
     TEST_ASSERT_NOT_EQUAL( NULL, xTimer );
+    TEST_ASSERT_EQUAL_PTR( &pxNewTimer, xTimer );
+}
 
-    /* HACK: Free the timer directly */
-    vPortFree( ( void * ) xTimer );
+void test_xTimerCreate_fail_timer_allocation( void )
+{
+    uint32_t ulID = 0;
+    TimerHandle_t xTimer = NULL;
+
+    pvPortMalloc_ExpectAndReturn(sizeof(Timer_t), NULL);
+
+    xTimer = xTimerCreate( "ut-timer",
+                           pdMS_TO_TICKS( 1000 ),
+                           pdTRUE,
+                           &ulID,
+                           xCallback_Test );
+
+    TEST_ASSERT_EQUAL( NULL, xTimer );
+}
+
+void test_xTimerCreate_fail_queue_allocation( void )
+{
+    uint32_t ulID = 0;
+    Timer_t  pxNewTimer;
+    TimerHandle_t xTimer = NULL;
+
+    pvPortMalloc_ExpectAndReturn(sizeof(Timer_t), &pxNewTimer);
+    vListInitialise_ExpectAnyArgs();
+    vListInitialise_ExpectAnyArgs();
+    xQueueGenericCreateStatic_ExpectAnyArgsAndReturn(NULL);
+    vListInitialiseItem_Ignore();
+
+    xTimer = xTimerCreate( "ut-timer",
+                           pdMS_TO_TICKS( 1000 ),
+                           pdTRUE,
+                           &ulID,
+                           xCallback_Test );
+
+    TEST_ASSERT_EQUAL( &pxNewTimer, xTimer );
 }
 
 void vApplicationGetTimerTaskMemory( StaticTask_t ** ppxTimerTaskTCBBuffer,
